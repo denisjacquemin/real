dc1d5660d760a78adad78dae89e69d6a1b57ecd1
Denis Jacquemin <denis.jacquemin@gmail.com>
2012-04-17 18:55:56 +0200

    adding scaffold for admin/agencies

---
+++ app/models/admin/agency.rb
@@ -0,0 +1,2 @@
+class Admin::Agency < ActiveRecord::Base
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120312173615) do
+ActiveRecord::Schema.define(:version => 20120417165257) do
+
+  create_table "admin_agencies", :force => true do |t|
+    t.string   "name"
+    t.string   "domain"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "pages", :force => true do |t|
     t.string   "title"
